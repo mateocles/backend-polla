d0d82b7e59712ed408c3ca7cb7f28ba5a827a390
mateocles <mateobermudez871@gmail.com>
2026-06-15 14:44:47 -0500

    changes

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -1,5 +1,13 @@
 const { PrismaClient } = require('@prisma/client');
 
-const prisma = new PrismaClient();
+// Singleton: en serverless (Vercel) evita crear múltiples clientes/conexiones
+// entre invocaciones reutilizando la instancia en el objeto global.
+const globalForPrisma = globalThis;
+
+const prisma = globalForPrisma.__prisma || new PrismaClient();
+
+if (!globalForPrisma.__prisma) {
+  globalForPrisma.__prisma = prisma;
+}
 
 module.exports = prisma;
